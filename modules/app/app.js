65843c3c49fa300702a71929c4dd67556714b9c1
xeviert <xevierturrubiartes@gmail.com>
2023-06-11 23:55:41 -0400

    got server running

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan');
-// const { NODE_ENV } = require('../../config');
+const { NODE_ENV } = require('../../config');
 
 
 const petRouter = require('../pets/pets.router')
 const peopleRouter = require('../people/people.router')
 
-// const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 const app = express()
 
-app.use(morgan());
+app.use(morgan(morganOption));
 app.use(cors());
 
 app.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./modules/app/app')
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
-  console.log(`[petful-server] Listening on ${ port }.`)
+  console.log(`[petful-server] Listening on ${port}.`)
 })
